327fbb4a1ab2b22c9ebe5bc6f96babc4d5fb4e8f
kostya-alch <kostafrompiter@gmail.com>
2021-11-05 16:53:32 +0300

    add component ThisDayInfo and Items

--- src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 import s from './ThisDayInfo.module.scss';
+import cloud from '../../../../assets/images/cloud.png';
+import { ThisDayItem } from './ThisDayItem';
 interface Props {}
 
+export interface Item {
+  icon_id: string;
+  name: string;
+  value: string;
+}
 export const ThisDayInfo = (props: Props) => {
-  return <div className={s.day}>ThisDayInfo</div>;
+  const items = [
+    {
+      icon_id: 'temp',
+      name: 'Температура',
+      value: '20° - ощущается как 17°',
+    },
+    {
+      icon_id: 'pressure',
+      name: 'Давление',
+      value: '765 мм ртутного столба - нормальное',
+    },
+    {
+      icon_id: 'precipitation',
+      name: 'Осадки',
+      value: 'Без осадков',
+    },
+    {
+      icon_id: 'wind',
+      name: 'Ветер',
+      value: '3 м/с юго-запад - легкий ветер',
+    },
+  ];
+  return (
+    <div className={s.this__day__info}>
+      <div className={s.this__day__info__items}>
+        {items.map((item: Item) => {
+          return <ThisDayItem key={item.icon_id} item={item} />;
+        })}
+      </div>
+      <img src={cloud} className={s.cloud__img} alt="облако" />
+    </div>
+  );
 };
