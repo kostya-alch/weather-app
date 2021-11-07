2f4a682f9ed42821d5ecc55ad72db86c546e0be6
kostya-alch <kostafrompiter@gmail.com>
2021-11-07 17:48:51 +0300

    add tabs component

--- src/pages/Home/components/Days/Tabs.tsx
+++ src/pages/Home/components/Days/Tabs.tsx
@@ -3,5 +3,29 @@ import s from './Days.module.scss';
 interface Props {}
 
 export const Tabs = (props: Props) => {
-  return <div></div>;
+  const tabs = [
+    {
+      value: 'На неделю',
+    },
+    {
+      value: 'На 10 дней',
+    },
+    {
+      value: 'На месяц',
+    },
+  ];
+  return (
+    <div className={s.tabs}>
+      <div className={s.tabs__wrapper}>
+        {tabs.map((tab) => {
+          return (
+            <div className={s.tab} key={tab.value}>
+              {tab.value}
+            </div>
+          );
+        })}
+      </div>
+      <div className={s.cancel}>Отменить</div>
+    </div>
+  );
 };
