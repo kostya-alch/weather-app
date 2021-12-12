8e3468cc66994cdc1c3693b8dedb86c696f6a16d
kostya-alch <kostafrompiter@gmail.com>
2021-12-12 14:59:54 +0300

    add real clock

--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
 import { Weather } from '../../../../store/types/types';
 import s from './ThisDay.module.scss';
+
 interface Props {
   weather: Weather;
 }
 
 export const ThisDay = ({ weather }: Props) => {
+  const [time, setTime] = useState(new Date().toLocaleString());
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setTime(new Date().toLocaleString()),
+      1000
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div className={s.this__day}>
       <div className={s.top__block}>
         <div className={s.top__block__wrapper}>
-          <div className={s.this__temp}>{Math.floor(weather.main.temp)}°</div>
+          <div className={s.this__temp}>{Math.round(weather.main.temp)}°</div>
           <div className={s.this__day__name}>Сегодня</div>
         </div>
         <GlobalSvgSelector id="sun" />
       </div>
       <div className={s.bottom__block}>
         <div className={s.this__time}>
-          Время: <span>{new Date().toLocaleTimeString()}</span>
+          Время: <span>{time}</span>
         </div>
         <div className={s.this__city}>
           Город: <span>{weather.name}</span>
